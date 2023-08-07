2f9ef332b93748ba4cfe91c3d047aa6e90b44909
Jeremy Frank <37932802+jfrank-summit@users.noreply.github.com>
2023-08-07 23:26:10 +0000

    add @polkadot/api

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,11 @@
+import { ApiPromise, WsProvider } from '@polkadot/api';
+
 const main = async () => {
-    console.log('hello world');
+    const ws = 'wss://rpc.devnet.subspace.network/ws';
+    const wsProvider = new WsProvider(ws);
+    const api = await ApiPromise.create({ provider: wsProvider });
+
+    console.log(api.genesisHash.toHex());
 };
 
 main().catch(console.error);
