805013e3fa7295707c55a1719af566ff96f91a6e
Jeremy Frank <jfrank@summitinvest.tech>
2023-08-08 18:25:46 -0600

    submit remark transaction

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,43 @@
-import { ApiPromise, WsProvider } from '@polkadot/api';
+import { ApiPromise, Keyring, WsProvider } from '@polkadot/api';
+import { cryptoWaitReady } from '@polkadot/util-crypto';
 
 const main = async () => {
-    const ws = 'wss://rpc.devnet.subspace.network/ws';
-    const wsProvider = new WsProvider(ws);
-    const api = await ApiPromise.create({ provider: wsProvider });
+    await cryptoWaitReady();
+    const keyring = new Keyring({ type: 'sr25519' });
+    const submitter = keyring.addFromUri(
+        process.env.SUBMITTER_SEED || '//Alice'
+    );
 
+    const ws = 'ws://127.0.0.1:9944';
+    const wsProvider = new WsProvider(ws);
+    const api = await ApiPromise.create({
+        provider: wsProvider,
+        noInitWarn: false,
+    });
+
+    const remark = await api.tx.system
+        .remark('test')
+        .signAndSend(submitter, ({ events = [], status, txHash }) => {
+            console.log(`Current status is ${status.type}`);
+
+            if (status.isInBlock) {
+                console.log(
+                    `Transaction included at blockHash ${status.asInBlock}`
+                );
+                console.log(`Transaction hash ${txHash.toHex()}`);
+
+                // Loop through Vec<EventRecord> to display all events
+                events.forEach(
+                    ({ phase, event: { data, method, section } }) => {
+                        console.log(
+                            `\t' ${phase}: ${section}.${method}:: ${data}`
+                        );
+                    }
+                );
+
+                remark();
+            }
+        });
     console.log(api.genesisHash.toHex());
 };
 
